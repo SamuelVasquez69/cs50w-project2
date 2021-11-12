a95bc3df23cb4e8574c1adec2f3f52f96a19c92f
Samuel Vásquez <vasquezsamuel643@gmail.com>
2021-11-11 19:35:43 -0600

    toque en el application.py

--- application.py
+++ application.py
@@ -1,13 +1,28 @@
 import os
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_session import Session
+from datetime import date, datetime
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins = '*')
 
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+## pa guardar los usuarios
+usuarios=[]
 
 @app.route("/")
 def index():
     return render_template("mensajes.html")
+
+@app.route("/login", method=['GET','POST'])
+def login():
+    session.clear()
+    usuario = request.form.get("username")
+    
